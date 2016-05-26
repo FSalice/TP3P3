ea69549f74e6d0a1552c024ea3397d3935b1cff4
Facu <Facu@Facu-PC>
2016-05-26 12:54:48 -0300

    generarInstancia() ahora toma solo un String ("tamañoDeGrafo y aristas")

--- TP3P3/src/TP2/SolucionTest.java
+++ TP3P3/src/TP2/SolucionTest.java
@@ -10,25 +10,30 @@ import org.junit.Test;
 public class SolucionTest {
 	
 	@Test
-	public void test() 
+	public void instanciaTest() 
 	{
-		fail("Not yet implemented");
+		Grafo g = generarInstancia("3 0,1,10 ; 1,2,10 ; 2,0,10");
+		Solucion s = Solucion.recorridoGoloso(g);
+		assertEquals(30,s.getLongitud());
 	}
 	
-	//las aristas se definen como "0,5,45;0,2,23;"(inicio, destino, peso)
-	private Grafo generarInstancia(int vertices, String aristas)
+	//las aristas se definen como "6 0,5,45;0,2,23;"(tamañoDeGrafo inicio, destino, peso;)
+	private Grafo generarInstancia(String aristas)
 	{
+		Pattern pat = Pattern.compile("\\d+");
+		Matcher m = pat.matcher(aristas);
 		//grafo vacio
 		Grafo ret = new Grafo();
 		//relleno con vertices truchos
-		for(int i = 0; i < vertices; i++)
+		if(!m.find())
+			throw new IllegalArgumentException("Grafo invalido!");
+		int tamaño = Integer.parseInt(m.group());
+		for(int i = 0; i < tamaño; i++)
 		{
-			ret.agregarEstacion(new Ciudad("", 0, 0));
+			ret.agregarEstacion(new Ciudad(""+i, 0, 0));
 		}
 		
 		//creo aristas con los pesos indicados
-		Pattern pat = Pattern.compile("\\d+");
-		Matcher m = pat.matcher(aristas);
 		int inicio, destino, peso;
 		while(m.find())
 		{
@@ -36,12 +41,11 @@ public class SolucionTest {
 			if(m.find())
 				destino = Integer.parseInt(m.group());
 			else
-				throw new IllegalArgumentException("Ultima arista incompleta");
+				throw new IllegalArgumentException("Ultima arista incompleta!");
 			if(m.find())
 				peso = Integer.parseInt(m.group());
 			else
-				throw new IllegalArgumentException("Ultima arista incompleta");
-			
+				throw new IllegalArgumentException("Ultima arista incompleta!");
 			ret.agregarArista(inicio, destino, peso);
 		}
 		return ret;
