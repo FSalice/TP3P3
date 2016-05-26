23f68e59809c444a26ad9c3ecb709ac4e46c39e2
Facu <Facu@Facu-PC>
2016-05-26 10:22:22 -0300

    Solucion.getRecorrido() Solucion.getLongitud()

--- TP3P3/src/TP2/Solucion.java
+++ TP3P3/src/TP2/Solucion.java
@@ -3,15 +3,26 @@ package TP2;
 public class Solucion {
 
 	private Grafo instancia;
-	//Recorrido representado en indices
+	// Recorrido representado en indices
 	private int[] recorrido;
-	//Longitud del recorrido
+	// Longitud del recorrido
 	private int longitud;
-	
-	//solucion "vacia"
-	private Solucion(Grafo g){
+
+	// solucion "vacia"
+	private Solucion(Grafo g) 
+	{
 		instancia = g;
 		recorrido = new int[instancia.getSize()];
 		longitud = Integer.MAX_VALUE;
+	}
+
+	public int[] getRecorrido() 
+	{
+		return recorrido;
+	}
+
+	public int getLongitud() 
+	{
+		return longitud;
 	}
 }
