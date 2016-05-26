aa5e35b919c91dc44c5798fae4756b0ca7697e83
Facu <Facu@Facu-PC>
2016-05-26 14:33:12 -0300

    SolucionTest mejoras al lenguaje: 	chequearLongitudGoloso 	chequearRecorridoGoloso

--- TP3P3/src/TP2/SolucionTest.java
+++ TP3P3/src/TP2/SolucionTest.java
@@ -2,6 +2,7 @@ package TP2;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,14 +11,41 @@ import org.junit.Test;
 public class SolucionTest {
 	
 	@Test
-	public void instanciaTest() 
+	public void longitudGolosoTest() 
 	{
-		Grafo g = generarInstancia("3 0,1,10 ; 1,2,10 ; 2,0,10");
-		Solucion s = Solucion.recorridoGoloso(g);
-		assertEquals(30,s.getLongitud());
+		chequearLongitudGoloso(30, "3 0,1,10 ; 1,2,10 ; 2,0,10");
+		chequearLongitudGoloso(40, "4 0,1,10 ; 1,2,10 ; 2,3,10; 3,0,10");
+		chequearLongitudGoloso(22, "4 0,1,10 ; 1,2,10 ; 2,3,10 ; 3,0,10 ; 0,2,1 ; 3,1,1" );
+		chequearLongitudGoloso(4, "4 0,1,1 ; 1,2,1 ; 2,3,1 ; 3,0,13 ; 0,2,1 ; 3,1,1" );
 	}
 	
-	//las aristas se definen como "6 0,5,45;0,2,23;"(tamañoDeGrafo inicio, destino, peso;)
+	private void chequearLongitudGoloso(int longitudEsperada, String grafo)
+	{
+		Grafo g = generarInstancia(grafo);
+		Solucion s = Solucion.recorridoGoloso(g);
+		assertEquals(longitudEsperada,s.getLongitud());
+	}
+	
+	@Test
+	public void recorridoGolosoTest() 
+	{
+		chequearRecorridoGoloso("0,2,1,0", "3 0,1,10 ; 1,2,10 ; 2,0,10");
+		chequearRecorridoGoloso("0,3,2,1,0", "4 0,1,10 ; 1,2,10 ; 2,3,10; 3,0,10");
+		chequearRecorridoGoloso("0,2,3,1,0", "4 0,1,10 ; 1,2,10 ; 2,3,10 ; 3,0,10 ; 0,2,1 ; 3,1,1" );
+	}
+	private void chequearRecorridoGoloso(String recorridoEsperado, String grafo)
+	{
+		Grafo g = generarInstancia(grafo);
+		Solucion s = Solucion.recorridoGoloso(g);
+		int[] esperado = recorrido(recorridoEsperado);
+		int[] obtenido = s.getRecorrido();
+		
+		for(int i = 0; i < obtenido.length; i++)
+			assertEquals(esperado[i],obtenido[i]);
+	}
+	
+	
+	//las aristas se definen como "6 : 0,5,45;0,2,23;"(tamañoDeGrafo : inicio, destino, peso;)
 	private Grafo generarInstancia(String aristas)
 	{
 		Pattern pat = Pattern.compile("\\d+");
@@ -48,6 +76,23 @@ public class SolucionTest {
 				throw new IllegalArgumentException("Ultima arista incompleta!");
 			ret.agregarArista(inicio, destino, peso);
 		}
+		return ret;
+	}
+	
+	private int[] recorrido(String a)
+	{
+		Pattern pat = Pattern.compile("\\d");
+		Matcher m = pat.matcher(a);
+		ArrayList<Integer> aux = new ArrayList<Integer>();
+		
+		while(m.find())
+			aux.add(Integer.parseInt(m.group()));
+		
+		int[] ret = new int[aux.size()];
+		
+		for(int i = 0; i < aux.size(); i++)
+			ret[i] = aux.get(i);
+		
 		return ret;
 	}
 }
